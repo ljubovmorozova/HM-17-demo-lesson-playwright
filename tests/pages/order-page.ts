a8a91068381b106919455f6bbdabdd2887c2b031
Ljubov Morozova <ljubov.morozova@askend.com>
2025-12-28 14:19:37 +0200

    HM-18-tests added

--- tests/pages/order-page.ts
+++ tests/pages/order-page.ts
@@ -1,37 +1,78 @@
-import { expect, Locator, Page } from '@playwright/test'
+import { expect, Locator, Page, test } from '@playwright/test'
+import BasePage from './base-page'
+import { SERVICE_URL } from '../../config/env-data'
 
-export class OrderPage {
-  readonly page: Page
+export class OrderPage extends BasePage {
   readonly statusButton: Locator
   readonly nameField: Locator
   readonly phoneField: Locator
   readonly commentField: Locator
   readonly createOrderButton: Locator
-  readonly successfulCreationPopup: Locator
   readonly logoutButton: Locator
 
-  constructor(page: Page) {
-    this.page = page
+  // creation popup
+  readonly successfulCreationPopup: Locator
+  readonly okButton: Locator
+  readonly codeSpan: Locator
+
+  // search popup elements
+  readonly searchOrderPopup: Locator
+  readonly orderIdInputField: Locator
+  readonly trackButton: Locator
+
+  constructor(page: Page, url?: string) {
+    super(page, url ? url : SERVICE_URL)
     this.statusButton = page.getByTestId('openStatusPopup-button')
     this.nameField = page.getByTestId('username-input')
     this.phoneField = page.getByTestId('phone-input')
     this.commentField = page.getByTestId('comment-input')
     this.createOrderButton = page.getByTestId('createOrder-button')
-    this.successfulCreationPopup = page.locator('main > .popup')
     this.logoutButton = page.getByTestId('logout-button')
+
+    this.successfulCreationPopup = page.locator('main > .popup')
+    this.okButton = this.successfulCreationPopup.getByTestId(
+      'orderSuccessfullyCreated-popup-ok-button',
+    )
+    this.codeSpan = this.successfulCreationPopup.locator('.notification-popup__text').nth(1)
+
+    this.searchOrderPopup = page.getByTestId('searchOrder-popup')
+    this.orderIdInputField = this.searchOrderPopup.getByTestId('searchOrder-input')
+    this.trackButton = this.searchOrderPopup.getByTestId('searchOrder-submitButton')
   }
 
   async checkInnerComponentsVisible(): Promise<void> {
-    await expect(this.statusButton).toBeVisible()
+    await this.checkElementVisibility(this.statusButton)
     await expect(this.statusButton).toBeEnabled()
-    await expect(this.nameField).toBeEnabled()
-    await expect(this.phoneField).toBeEnabled()
-    await expect(this.commentField).toBeEnabled()
-    await expect(this.createOrderButton).toBeEnabled()
+    await this.checkElementVisibility(this.nameField)
+    await this.checkElementVisibility(this.phoneField)
+    await this.checkElementVisibility(this.commentField)
+    await this.checkElementVisibility(this.createOrderButton)
   }
+
   async checkCreationPopupVisible(visible = true): Promise<void> {
-    await expect(await this.successfulCreationPopup.getAttribute('class')).toContain(
-      visible ? 'popup_opened' : 'undefined',
+    expect(await this.successfulCreationPopup.getAttribute('class')).toContain(
+      visible ? 'popup popup_opened' : 'undefined',
     )
+  }
+
+  async findOrderById(id: number): Promise<void> {
+    await test.step(`Search order by ID: '${id}'`, async () => {
+      await this.clickElement(this.statusButton)
+      await this.fillElement(this.orderIdInputField, String(id))
+      await this.clickElement(this.trackButton)
+    })
+  }
+
+  async closeCreationPopup(): Promise<void> {
+    await test.step('Close popup after order creation', async () => {
+      await this.clickElement(this.okButton)
+    })
+  }
+
+  async getOrderIdFromPopup(): Promise<number> {
+    const text = await this.codeSpan.innerText()
+    const strArray = text.split(' ')
+
+    return Number(strArray[strArray.length - 1])
   }
 }
--- tests/ui/e2e-flow-with-before.spec.ts
+++ tests/ui/e2e-flow-with-before.spec.ts
@@ -1,44 +1,65 @@
 import { test, expect } from '@playwright/test'
 import { LoginPage } from '../pages/login-page'
 import { faker } from '@faker-js/faker/locale/ar'
-import { PASSWORD, USERNAME } from '../../config/env-data'
+import { PASSWORD, SERVICE_URL, USERNAME } from '../../config/env-data'
+import NotFoundPage from '../pages/not-found-page'
+import { OrderPage } from '../pages/order-page'
+import FoundPage from '../pages/found-page'
 
-let loginPage: LoginPage
+let authPage: LoginPage
 
 test.beforeEach(async ({ page }) => {
-  loginPage = new LoginPage(page)
-  await loginPage.open()
+  authPage = new LoginPage(page)
+  await authPage.open()
 })
 
 test('TL-17-3 signIn button disabled when incorrect data inserted', async ({}) => {
-  await loginPage.usernameField.fill(faker.lorem.word(2))
-  await loginPage.passwordField.fill(faker.lorem.word(7))
-  await expect(loginPage.signInButton).toBeDisabled()
-})
-
-test.skip('TL-17-4 error message displayed when incorrect credentials used', async ({}) => {
-  // do not implement test
+  await authPage.usernameField.fill(faker.lorem.word(3))
+  await authPage.passwordField.fill(faker.lorem.word(9))
+  await expect(authPage.signInButton).toBeEnabled()
 })
 
 test('TL-17-5 login with correct credentials and verify order creation page', async ({}) => {
-  const orderCreationPage = await loginPage.signIn(USERNAME, PASSWORD)
+  const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
   await expect(orderCreationPage.statusButton).toBeVisible()
   await orderCreationPage.checkInnerComponentsVisible()
 })
 
 test('TL-17-6 login and create order', async ({ page }) => {
-  const orderCreationPage = await loginPage.signIn(USERNAME, PASSWORD)
-  await orderCreationPage.nameField.fill('test')
-  await orderCreationPage.phoneField.fill('test1234')
-  await orderCreationPage.commentField.fill('1234')
+  const foundPage = new FoundPage(page)
+  const orderInfo = {
+    name: 'test',
+    phoneField: '20252026',
+    comment: 'comment',
+  }
+
+  const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
+  await orderCreationPage.nameField.fill(orderInfo.name)
+  await orderCreationPage.phoneField.fill(orderInfo.phoneField)
+  await orderCreationPage.commentField.fill(orderInfo.comment)
   await orderCreationPage.checkCreationPopupVisible(false)
   await orderCreationPage.createOrderButton.click()
   await page.waitForTimeout(1000)
   await orderCreationPage.checkCreationPopupVisible(true)
+  const orderId = await orderCreationPage.getOrderIdFromPopup()
+  await orderCreationPage.closeCreationPopup()
+  await orderCreationPage.findOrderById(orderId)
+  await foundPage.checkElementVisibility(foundPage.orderName)
 })
 
-test('TL-17-7 logout', async ({}) => {
-  const orderCreationPage = await loginPage.signIn(USERNAME, PASSWORD)
+test('TL-17-7 logout', async () => {
+  const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
+  await expect(orderCreationPage.logoutButton).toBeVisible()
   await orderCreationPage.logoutButton.click()
-  await expect(loginPage.signInButton).toBeVisible()
+  await expect(authPage.signInButton).toBeVisible()
+})
+
+test('TL-18-1 Check not found page', async ({page}) => {
+  const notFoundPage = new NotFoundPage(page, `${SERVICE_URL}/order/-1`)
+  const orderPage = new OrderPage(page)
+
+  await authPage.signIn(USERNAME, PASSWORD)
+  await orderPage.findOrderById(-1)
+  await notFoundPage.checkElementVisibility(notFoundPage.title)
+  await notFoundPage.checkElementVisibility(notFoundPage.description)
 })
